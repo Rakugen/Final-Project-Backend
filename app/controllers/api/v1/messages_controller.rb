757544abf7ed3e32321138ce2a282ee9294175a9
Rakugen <simonlee0306@gmail.com>
2019-03-04 18:14:09 -0500

    fixed action cable by adding an unsubscribe function

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      ActionCable.server.broadcast("message_channel", @message)
+      MessageChannel.broadcast_to(@message.chatroom, @message)
       render json: @message, status: :created
     else
       render json: {error: 'Could not create message.'}, status: 422
